cae10700ffe934cf3b9d68134efcc298d3c35041
abdielreyhan <abdielreyhan>
2024-03-25 13:52:35 +0700

    Fixing nama channel

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const io = require('socket.io')(server,{
 io.on('connection',(socket)=>{
     console.log('connection');
 
-    socket.on('sendChatToServer',(message)=>{
+    socket.on('sendAlertToServer',(message)=>{
         console.log(message);
-        io.sockets.emit('sendChatToClient',message);
+        io.sockets.emit('sendAlertToClient',message);
         // socket.broadcast.emit('sendChatToClient',message);
     });
 
-    socket.on('sendPenjualanOlehToServer',(message)=>{
-        console.log(message);
-        io.sockets.emit('sendPenjualanOlehToClient',message);
-        // socket.broadcast.emit('sendChatToClient',message);
-    });
+    // socket.on('sendPenjualanOlehToServer',(message)=>{
+    //     console.log(message);
+    //     io.sockets.emit('sendPenjualanOlehToClient',message);
+    //     // socket.broadcast.emit('sendChatToClient',message);
+    // });
     
-    socket.on('sendTestPrint',(message)=>{
-        console.log(message);
-        io.sockets.emit('sendTestPrintClient',message);
-        // socket.broadcast.emit('sendChatToClient',message);
-    });
+    // socket.on('sendTestPrint',(message)=>{
+    //     console.log(message);
+    //     io.sockets.emit('sendTestPrintClient',message);
+    //     // socket.broadcast.emit('sendChatToClient',message);
+    // });
 
     socket.on('disconnect',(socket)=>{
         console.log('disconnect');
